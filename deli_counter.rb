a7d5efc43a829e32049a8693935432cad83604a5
Scott Hoisington <hoyzington@gmail.com>
2019-12-11 21:39:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,4 +4,16 @@ def line(customers)
   if customers.size == 0
     return "The line is currently empty."
   else
-    
+    return "The line is currently: "
+  end
+end
+
+
+def take_a_number
+  
+end
+
+
+def now_serving
+  
+end
