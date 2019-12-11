f5892d4ab210634337bdade8e6fd39f630ab4f80
Scott Hoisington <hoyzington@gmail.com>
2019-12-11 21:43:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,11 @@
 katz_deli = []
 
 def line(customers)
-  if customers.size == 0
+  if customers.count == 0
     return "The line is currently empty."
   else
+    place_in_line = 1
+    while place_in_line <= customers.count
     return "The line is currently: "
   end
 end
